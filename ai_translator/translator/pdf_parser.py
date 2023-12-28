6e814e1053ff4bfde8fb7a8079816589eadc630f
zhengyuntao123 <704344527@qq.com>
2023-12-28 18:03:05 -0500

    pdf translator

---
+++ ai_translator/translator/pdf_parser.py
@@ -0,0 +1,64 @@
+import pdfplumber
+from typing import Optional
+from book import Book, Page, Content, ContentType, TableContent
+from translator.exceptions import PageOutOfRangeException
+from utils import LOG
+
+
+class PDFParser:
+    def __init__(self):
+        pass
+
+    def parse_pdf(self, pdf_file_path: str, pages: Optional[int] = None) -> Book:
+        book = Book(pdf_file_path)
+
+        with pdfplumber.open(pdf_file_path) as pdf:
+            if pages is not None and pages > len(pdf.pages):
+                raise PageOutOfRangeException(len(pdf.pages), pages)
+
+            if pages is None:
+                pages_to_parse = pdf.pages
+            else:
+                #翻译到第pages面
+                pages_to_parse = pdf.pages[:pages]
+
+            for pdf_page in pages_to_parse:
+                page = Page()
+
+                # Store the original text content
+                # 这里并没有保持原始布局
+                raw_text = pdf_page.extract_text()
+                tables = pdf_page.extract_tables()
+
+                # Remove each cell's content from the original text
+                for table_data in tables:
+                    for row in table_data:
+                        for cell in row:
+                            # 因为extract_text也会解析出表格
+                            # 所以通过该循环暴力地把文本中的表格内容替换为空
+                            # replace默认替换所有出现，此处的1表示替换raw_text中cell的第一次出现
+                            raw_text = raw_text.replace(cell, "", 1)
+
+                # Handling text
+                if raw_text:
+                    # Remove empty lines and leading/trailing whitespaces
+                    raw_text_lines = raw_text.splitlines()
+                    cleaned_raw_text_lines = [line.strip() for line in raw_text_lines if line.strip()]
+                    cleaned_raw_text = "\n".join(cleaned_raw_text_lines)
+
+                    # content类型是ContentType.TEXT
+                    text_content = Content(content_type=ContentType.TEXT, original=cleaned_raw_text)
+                    page.add_content(text_content)
+                    LOG.debug(f"[raw_text]\n {cleaned_raw_text}")
+
+
+
+                # Handling tables
+                if tables:
+                    table = TableContent(tables)
+                    page.add_content(table)
+                    LOG.debug(f"[table]\n{table}")
+
+                book.add_page(page)
+
+        return book
---
+++ tests/test_translated.md
@@ -0,0 +1,20 @@
+测试数据
+这个数据集包含由OpenAI的AI语言模型ChatGPT提供的两个测试样本。
+这些样本包括一个Markdown表格和一个英文文本段落，可用于测试支持文本和表格格式的英译中翻译软件。
+文本测试
+快速的棕色狐狸跳过懒狗。这个每个字母至少包含一次的句子是一种使用全部英文字母的句子。Pangram经常用于测试字体、键盘和其他与文本相关的工具。除了英语之外，还有很多其他语言的Pangram。由于语言的独特特点，有些Pangram更难构造。
+表格测试
+
+| 水果 | 颜色 | 价格（USD） |
+| --- | --- | --- |
+| Apple | Red | 1.20 |
+| Banana | Yellow | 0.50 |
+| Orange | Orange | 0.80 |
+| Strawberry | Red | 2.50 |
+| Blueberry | Blue | 3.00 |
+| Kiwi | Green | 1.00 |
+| Mango | Orange | 1.50 |
+| Grape | Purple | 2.00 |
+
+---
+
